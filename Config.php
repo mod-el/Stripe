cd29c5ece916a773d7b34a72f501e2104797442f
Domenico Giambra <sviluppo@editt.it>
2020-06-18 16:50:41 +0200

    Initial commit

---
+++ Config.php
@@ -0,0 +1,43 @@
+<?php namespace Model\Stripe;
+
+use Model\Core\Module_Config;
+
+class Config extends Module_Config
+{
+	/**
+	 */
+	protected function assetsList()
+	{
+		$this->addAsset('config', 'config.php', function () {
+			return '<?php
+$config = [
+	\'path\' => \'stripe\',
+	\'publishable-key\' => \'\',
+	\'secret-key\' => \'\',
+	\'success-path\' => \'payment-success\',
+	\'cancel-path\' => \'payment-cancel\',
+	\'webhook-secret\' => \'\',
+	\'handle-payment\' => function (?string $orderId, float $amount) {
+	},
+];
+';
+		});
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getRules(): array
+	{
+		$config = $this->retrieveConfig();
+
+		return [
+			'rules' => [
+				'stripe' => $config['path'],
+			],
+			'controllers' => [
+				'Stripe',
+			],
+		];
+	}
+}
---
+++ Controllers/StripeController.php
@@ -0,0 +1,60 @@
+<?php namespace Model\Stripe\Controllers;
+
+use Model\Core\Controller;
+
+class StripeController extends Controller
+{
+	public function index()
+	{
+		switch ($this->model->getRequest(1)) {
+			case 'hook':
+				$config = $this->model->_Stripe->retrieveConfig();
+				\Stripe\Stripe::setApiKey($config['secret-key']);
+
+				// If you are testing your webhook locally with the Stripe CLI you
+				// can find the endpoint's secret by running `stripe listen`
+				// Otherwise, find your endpoint's secret in your webhook settings in the Developer Dashboard
+				$endpoint_secret = $config['webhook-secret'];
+
+				$payload = @file_get_contents('php://input');
+				$sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+				$event = null;
+
+				try {
+					$event = \Stripe\Webhook::constructEvent(
+						$payload, $sig_header, $endpoint_secret
+					);
+				} catch (\UnexpectedValueException $e) {
+					// Invalid payload
+					http_response_code(400);
+					exit();
+				} catch (\Stripe\Exception\SignatureVerificationException $e) {
+					// Invalid signature
+					http_response_code(400);
+					exit();
+				}
+
+				// Handle the event
+				switch ($event->type) {
+					case 'checkout.session.completed':
+						$stripeObject = $event->data->object;
+						try {
+							$config['handle-payment']($stripeObject->client_reference_id ?? $stripeObject->metadata['orderId'] ?? null, $stripeObject->amount);
+						} catch (\Exception $e) {
+							http_response_code(500);
+							echo getErr($e);
+						}
+						break;
+					default:
+						// Unexpected event type
+						http_response_code(400);
+				}
+				break;
+			default:
+				http_response_code(400);
+				break;
+		}
+
+		die();
+	}
+}
